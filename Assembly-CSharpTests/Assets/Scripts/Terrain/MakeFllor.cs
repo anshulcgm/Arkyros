f9982012b6e221407f931fce75096a4c87a3f8eb
SirAJn8r <hyperactive@live.com>
2018-12-08 20:39:38 -0800

    fixing files

--- Assembly-CSharpTests/Assets/Scripts/Terrain/MakeFllor.cs
+++ Assembly-CSharpTests/Assets/Scripts/Terrain/MakeFllor.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class PlanetMaker
 {
     public static void MakePlanet(float radius, float variance, int varianceSeed, int numPointsLat, int numPointsLong, out List<Vector3> points, out List<int[]> cons)
-        //radius is (minimum) radius of the planet AKA distance from core to surface corners/points
-        //variance is maximum radius - minimum, AKA variance is maximum radius added at any 1 point/corner
-        //variance seed is just the random seed
-        //numPointsLat is number of points around (horizontally) on the sphere
-        //numPointsLong is number of layers of points (layers horizontal and vertical in a way too)
-        //points is an output bascially
-        //cons (connections) is another output basically
+    //radius is (minimum) radius of the planet AKA distance from core to surface corners/points
+    //variance is maximum radius - minimum, AKA variance is maximum radius added at any 1 point/corner
+    //variance seed is just the random seed
+    //numPointsLat is number of points around (horizontally) on the sphere
+    //numPointsLong is number of layers of points (layers horizontal and vertical in a way too)
+    //points is an output bascially
+    //cons (connections) is another output basically
     {
         points = new List<Vector3>();
         cons = new List<int[]>();
@@ -23,9 +23,9 @@ public class PlanetMaker
         {
             for (double rot = 0; rot < 2 * Math.PI; rot += 2 * Math.PI / numPointsLat)
             {
-                float x = (float)(Math.Cos(rot) * Math.Cos(theta) * (radius + rand.NextDouble() * variance) );
-                float y = (float)(Math.Sin(theta) *                 (radius + rand.NextDouble() * variance) );
-                float z = (float)(Math.Sin(rot) * Math.Cos(theta) * (radius + rand.NextDouble() * variance) );
+                float x = (float)(Math.Cos(rot) * Math.Cos(theta) * (radius + rand.NextDouble() * variance));
+                float y = (float)(Math.Sin(theta) * (radius + rand.NextDouble() * variance));
+                float z = (float)(Math.Sin(rot) * Math.Cos(theta) * (radius + rand.NextDouble() * variance));
                 points.Add(new Vector3(x, y, z));
             }
         }
@@ -43,7 +43,7 @@ public class PlanetMaker
             cons.Add(new int[] { 0, i });
         }
         //run through all but bottom point and top cirlce
-        for(int i = 1; i < (points.Count - numPointsLat - 1); i++)
+        for (int i = 1; i < (points.Count - numPointsLat - 1); i++)
         {
             if (i % numPointsLat == numPointsLat - 1) //if last in rot
             //connect all but top cricle last in rot        --connect +numPointsLat and +1-numPointsLat
@@ -66,7 +66,7 @@ public class PlanetMaker
             cons.Add(new int[] { i, i + 1 });
         }
         //connect top circle last in rot                --connect last and +1-numPointsLat
-        cons.Add(new int[] { points.Count - 2, points.Count - 1});
+        cons.Add(new int[] { points.Count - 2, points.Count - 1 });
         cons.Add(new int[] { points.Count - 2, points.Count - numPointsLat });
     }
 }
