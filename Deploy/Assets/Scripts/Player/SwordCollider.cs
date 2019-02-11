3bf69e26ebaa890f8548e7c2f1059089cce772a4
YU, RENE <s-RYU@lwsd.org>
2019-02-11 12:19:27 -0800

    CRISIS AVERTED made box collider for sword and a new scene for it

---
+++ Deploy/Assets/Scripts/Player/SwordCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCollider : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject); //other.takeDamage();
+        }
+    }
+}
