93cf91a9ce7bdc8da1d0635674e0cd7d0bd0931b
shuhua <wishd8@gmail.com>
2019-02-11 12:12:50 -0800

    Base Ability class

---
+++ Deploy/Assets/Scripts/Player/BaseAbility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAbility {
+
+    protected int ID;
+    protected int prereqID;
+    protected double cooldown;
+    protected double cost;
+
+    public void runAbility()
+    {
+
+    }	
+
+}
