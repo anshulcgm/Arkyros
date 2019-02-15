903b047901c74414bc70d7c2bc53f140a3fac133
Toma Itagaki <tomaitagaki@gmail.com>
2019-02-15 12:44:34 -0800

    Simple Attack Anim Added

--- Deploy/Assets/Scripts/Enemies/Behavior/DetectPlayer.cs
+++ Deploy/Assets/Scripts/Enemies/Behavior/DetectPlayer.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 //Toma Itagaki
 public class DetectPlayer : MonoBehaviour {
 
+    public GameObject player;
+
     private Vector3 playerPosition;
     private Vector3 enemyPosition;
-
+     
     private Vector3 direction;
     private float distance;
 
+    public void Start() {
+        playerPosition = player.transform.position;
+        enemyPosition = transform.position;
+    }
 
     public void findDistance()
     {
         this.distance = Vector3.Distance(playerPosition, enemyPosition);
         this.direction = (enemyPosition - playerPosition).normalized;
 
-        if (distance < 10)
+        if (distance < 5)
         {
             move(direction);
         }
@@ -24,6 +30,6 @@ public class DetectPlayer : MonoBehaviour {
 
     public void move(Vector3 direction)
     {
-        //move function
+        //move funct
     }
 }
