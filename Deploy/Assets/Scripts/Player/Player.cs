d67df541cf275a00fe744477633455407212e2f2
YU, RENE <s-RYU@lwsd.org>
2019-02-17 13:04:49 -0800

    player class weapons and abilities access

--- Deploy/Assets/Scripts/Player/Player.cs
+++ Deploy/Assets/Scripts/Player/Player.cs
@@ -5,6 +5,9 @@ public class Player
     public static int EXP;
     public static int LEVEL;
 
+    private static int Weapon; //might be public
+    private static int[] Abilities;
+
 	public Player()
 	{
         EXP = 1;
@@ -20,5 +23,15 @@ public class Player
     public static void UpdateLevel()
     {
         LEVEL = (int)Math.Sqrt(EXP);
+    }
+
+    public static void ChangeWeapon(int newWeaponID)
+    {
+        Weapon = newWeaponID;
+    }
+
+    public static void ChangeAbility(int position, int abilityID)
+    {
+        Abilities[position] = abilityID;
     }
 }
