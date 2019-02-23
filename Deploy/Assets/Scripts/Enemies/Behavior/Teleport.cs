9503abd2aafc6171d49ba1c3a2dd47abd336c4ef
Lucas Sanchez <legendofdunsparce88@gmail.com>
2019-02-23 16:37:39 -0600

    Teleport Script

---
+++ Deploy/Assets/Scripts/Enemies/Behavior/Teleport.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Lucas Sanchez
+
+
+public class Teleport : MonoBehaviour {
+
+    public Vector3 newPosition;
+
+    // Use this for initialization
+    void Start () {
+        this.transform.position = newPosition;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
