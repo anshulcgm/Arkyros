a8adc513739bb0b4669f0af2fba9350ff836c7e0
James <jamesjoko@comcast.net>
2019-03-03 15:34:12 -0800

    Kamikaze Attack

---
+++ Deploy/Assets/PlayerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public GameObject player;
+    private Rigidbody r;
+    public float speed;
+    public float distance;
+
+	// Use this for initialization
+	void Start () {
+        r = GetComponent<Rigidbody>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerPos = player.transform.position;
+        Vector3 enemyPos = this.gameObject.transform.position;
+        if (Vector3.Distance(playerPos, enemyPos) < distance){
+            r.velocity = (playerPos - enemyPos).normalized * speed;
+        }
+        else
+        {
+            r.velocity = Vector3.zero;
+        }
+    }
+}
