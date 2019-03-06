0a3b081c4394a3c2ddb08d67cf0fadb46e5ffe9d
Lucas Sanchez <legendofdunsparce88@gmail.com>
2019-03-05 21:41:39 -0600

    Knockback for Golem thing

---
+++ Deploy/Assets/KnockbackMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class KnockbackMove : MonoBehaviour {
+
+    private Rigidbody rb;
+    public GameObject enemy;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Vector3 enemyPos = enemy.transform.position;
+            Vector3 direction = (this.transform.position - enemyPos).normalized;
+            rb.velocity = direction*speed;
+        }
+    }
+}
