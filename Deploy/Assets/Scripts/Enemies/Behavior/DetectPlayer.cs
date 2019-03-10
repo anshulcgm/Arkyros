8124dcf2092e73a9a9633bb97ea09fa6f0026041
Anshulcgm <anshul.cgm@gmail.com>
2019-03-10 12:15:53 -0700

    Houskeeping after merging Task 3 and deleting unnecessary scenes

--- Deploy/Assets/Scripts/Enemies/Behavior/DetectPlayer.cs
+++ Deploy/Assets/Scripts/Enemies/Behavior/DetectPlayer.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 //Toma Itagaki
 public class DetectPlayer : MonoBehaviour {
 
+    public GameObject player;
+    private Animator anim;
+
     private Vector3 playerPosition;
     private Vector3 enemyPosition;
-
+     
     private Vector3 direction;
     private float distance;
 
+    public void Start() {
+        playerPosition = player.transform.position;
+        enemyPosition = transform.position;
+
+        anim = GetComponent<Animator>();
+    }
+
+    public void Update() {
+        findDistance();
+    }
 
     public void findDistance()
     {
         this.distance = Vector3.Distance(playerPosition, enemyPosition);
         this.direction = (enemyPosition - playerPosition).normalized;
 
-        if (distance < 10)
+        if (distance < 5)
         {
-            move(direction);
+            anim.SetTrigger("AttackOnSight");
         }
     }
-
-    public void move(Vector3 direction)
-    {
-        //move function
-    }
+    
 }
