312ec9829a7657e9f939dd59f5895b8617b2a423
Shiven Bhatt <shivenbhatt@outlook.com>
2019-03-17 16:58:52 -0700

    removing compilation errors

--- Deploy/Assets/StandardAssets/Utility/EventSystemChecker.cs
+++ Deploy/Assets/StandardAssets/Utility/EventSystemChecker.cs
@@ -10,12 +10,6 @@ public class EventSystemChecker : MonoBehaviour
 	// Use this for initialization
 	void Awake ()
 	{
-	    if(!FindObjectOfType<EventSystem>())
-        {
-           //Instantiate(eventSystem);
-            GameObject obj = new GameObject("EventSystem");
-            obj.AddComponent<EventSystem>();
-            obj.AddComponent<StandaloneInputModule>().forceModuleActive = true;
-        }
+
 	}
 }
