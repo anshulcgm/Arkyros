21957ded383d72322485e3f5bf3f68621195ed35
YU, RENE <s-RYU@lwsd.org>
2019-04-25 21:28:30 -0700

    added template, darkflight

---
+++ Deploy/Assets/Scripts/Player/Abilities/Darkflight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darkflight : MonoBehaviour
+{
+    public float cooldown;
+
+    private GameObject camera;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("e") && cooldown == 0)      //place key, any key can be pressed.
+        {
+            //start = DateTime.Now;
+            anim.SetBool("NAME OF ANIMATION", true); //this tells the animator to play the right animation
+            cooldown = 240;                          //placeholder time, divide by 60 for cooldown in seconds
+
+
+            rigidbody.AddForce(transform.up * 1000, ForceMode.Impulse); //jumps super high
+            //reee flying code
+        }
+
+        if (cooldown > 0) //counts down for the cooldown
+        {
+            cooldown--;
+        }
+    }
+}
---
+++ Deploy/Assets/Scripts/Player/Abilities/Template.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Template : MonoBehaviour
+{
+    public float cooldown = 0;
+
+    private GameObject camera;
+
+    private Animator anim;
+
+    //DateTime start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("e") && cooldown == 0)      //place key, any key can be pressed.
+        {
+            //start = DateTime.Now;
+            anim.SetBool("NAME OF ANIMATION", true); //this tells the animator to play the right animation
+            cooldown = 240;                          //placeholder time, divide by 60 for cooldown in seconds
+
+
+
+            /*
+             * All the code for the ability that you want to write
+             * transform.forward for the direction the player is 
+             * maybe setting colliders
+             * instantiating new objects
+             * to damage enemy, EnemyGameObject.GetComponent<StatManager>().changeHealth(amount), amount can be positive or negative
+             */
+        }
+
+
+
+
+
+
+        if (cooldown > 0) //counts down for the cooldown
+        {
+            cooldown--;
+        }
+    }
+}
