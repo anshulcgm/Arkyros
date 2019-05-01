b96ba0ff342294152065c9c03839a3894ea2de7a
Anshulcgm <anshul.cgm@gmail.com>
2019-04-30 20:33:01 -0700

    All errors finally fixed

--- Deploy/Assets/Scripts/Enemies/Behavior/IrradiatedEnemies/IrradiatedEnemies.cs
+++ Deploy/Assets/Scripts/Enemies/Behavior/IrradiatedEnemies/IrradiatedEnemies.cs
@@ -18,29 +18,30 @@ public class IrradiatedEnemies: Enemy
 
 
 
+
     //Constructor
-    public IrradiatedEnemies(float maxhp, int ms, float defense, GameObject referenceObject, float radius, float speedBuff, float hpBuff, float attackBuff, float pAttackDebuff, float pSpeedDebuff) : base(maxhp, ms, defense,referenceObject)
+    public IrradiatedEnemies(float maxhp, int ms, float defense, GameObject referenceObject, float radius, float speedBuff, float hpBuff, float attackBuff, float pAttackDebuff, float pSpeedDebuff) : base(maxhp, ms, defense, referenceObject)
     {
-            this.radius = radius;
-            this.speedBuff = speedBuff;
-            this.hpBuff = hpBuff;
-            this.attackBuff = attackBuff;
-            this.pAttackDebuff = pAttackDebuff;
-            this.pSpeedDebuff = pSpeedDebuff;
+        this.radius = radius;
+        this.speedBuff = speedBuff;
+        this.hpBuff = hpBuff;
+        this.attackBuff = attackBuff;
+        this.pAttackDebuff = pAttackDebuff;
+        this.pSpeedDebuff = pSpeedDebuff;
     }
 
 
     public void spawnEnemy(Dictionary<GameObject, int> enemiesToSpawn, float radius)
     {
-        //The dictionary represents the various enemies that will be spawned and the key is how many of them will be spawned
+
         if (enemyStats.getHealth() < enemyStats.getMaxHealth() * 0.1)
         {
-        //Spawn code for other enemies - Anshul Task 6
-        float maxHPProportion = Random.Range(1, 101) * 0.01f; //Randomizes how much health they will have
-                
-        foreach (KeyValuePair<GameObject, int> kvp in enemiesToSpawn)
+            //Spawn code for other enemies - Anshul Task 6
+            float maxHPProportion = Random.Range(1, 101) * 0.01f;
+
+            foreach (KeyValuePair<GameObject, int> kvp in enemiesToSpawn)
             {
-                for(int i = 0; i < kvp.Value; i++)
+                for (int i = 0; i < kvp.Value; i++)
                 {
                     RandomEnemySpawn.spawnEnemyWithinRadius(kvp.Key, radius, referenceObject.transform.position, maxHPProportion);
                 }
@@ -48,52 +49,36 @@ public class IrradiatedEnemies: Enemy
         }
     }
 
-    public void updateMovement()
-    {
-        if (base.hp < base.maxhp * 0.2)
-        { //if IR enemy health is lower than 20% make it move
-            ms = 10; //place holder value
-        }
-        else
-        {
-            ms = 0;
-        } //add clause for when it is far from player
-    }
 
-
-    public void spawnEnemy()
-    {
-        if (base.hp < base.maxhp * 0.1)
-        { //if IR enemy health is lower than 10% spawn other enemies
-            //spawn function
-        }
-    }
 
     public void updateEnemiesAttack()
-    { //determines whether an object is in the radius of irradiated enemy
+    {
         Collider[] hitColliders = Physics.OverlapSphere(referenceObject.transform.position, radius);
         int i = 0;
         while (i < hitColliders.Length)
         {
             if (hitColliders[i].CompareTag("Enemy"))
-            { //if the colliding object is "enemy" tagged
-                hitColliders[i].gameObject.attack += attackBuff; //increase attack stat
+            {
+                //hitColliders[i].gameObject.attack += attackBuff;
+                //Will write code for this when we can change specialized values of enemy classes
+
             }
             i++;
         }
     }
 
     public void updateEnemiesHP()
-    {  //determines whether an object is in the radius of irradiated enemy
+    {
         Collider[] hitColliders = Physics.OverlapSphere(referenceObject.transform.position, radius);
         int i = 0;
         while (i < hitColliders.Length)
         {
-            if (hitColliders[i].CompareTag("Enemy"))  //if the colliding object is "enemy" tagged
+            if (hitColliders[i].CompareTag("Enemy"))
             {
-                if (maxhpBuff != 0)
+                if (hpBuff != 0)
                 {
-                    hitColliders[i].gameObject.maxhp *= maxhpBuff;  //increase the max health of enemy
+                    //hitColliders[i].gameObject.maxhp *= maxhpBuff;
+                    hitColliders[i].gameObject.GetComponent<StatManager>().changeHealth(hpBuff);
                 }
 
             }
@@ -101,29 +86,16 @@ public class IrradiatedEnemies: Enemy
         }
     }
 
-    public void updateEnemiesReload()
-    {  //determines whether an object is in the radius of irradiated enemy
-        Collider[] hitColliders = Physics.OverlapSphere(referenceObject.transform.position, radius);
-        int i = 0;
-        while (i < hitColliders.Length)
-        {
-            if (hitColliders[i].CompareTag("Enemy")) //if the colliding object is "enemy" tagged
-            {
-                hitColliders[i].gameObject.reload -= reloadBuff;  //decrease the reload speed
-            }
-            i++;
-        }
-    }
 
     public void updateEnemiesSpeed()
-    {  //determines whether an object is in the radius of irradiated enemy
+    {
         Collider[] hitColliders = Physics.OverlapSphere(referenceObject.transform.position, radius);
         int i = 0;
         while (i < hitColliders.Length)
         {
-            if (hitColliders[i].CompareTag("Enemy")) //if the colliding object is "enemy" tagged
+            if (hitColliders[i].CompareTag("Enemy"))
             {
-                hitColliders[i].gameObject.speed += speedBuff; //increase speed of enemy
+                hitColliders[i].gameObject.GetComponent<StatManager>().enemyMultiplySpeed(speedBuff);
             }
             i++;
         }
@@ -138,6 +110,7 @@ public class IrradiatedEnemies: Enemy
         //mutation of the irradiation area
         radius = radius * factor;
     }
+
     public float geHpBuff()
     {
         return hpBuff;
@@ -151,6 +124,7 @@ public class IrradiatedEnemies: Enemy
         return attackBuff;
     }
 
-    
+
+
 }
 
