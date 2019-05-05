3147eb6f9826356820cc411b948d93265c55f6f9
GONCHARENKO, ANNA <s-ANGONCHARENKO@lwsd.org>
2019-05-04 21:34:34 -0700

    passive dictionary and template, soul reaper

---
+++ Deploy/Assets/Scripts/Player/Abilities/PassiveTemplate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveTemplate : Passive
+{
+    //ADD passive ability's tier - check from ability log
+    public int tier = 0; 
+
+    public override void On()
+    {
+        //code for ability
+    }
+    public override void Off()
+    {
+        //reverse ability's effects
+    }
+}
---
+++ Deploy/Assets/Scripts/Player/Abilities/SoulReaper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulReaper : Passive
+{
+    public int tier = 6;
+
+    public override void On()
+    {
+        //code for ability
+    }
+    public override void Off()
+    {
+        //reverse ability's effects
+    }
+}
