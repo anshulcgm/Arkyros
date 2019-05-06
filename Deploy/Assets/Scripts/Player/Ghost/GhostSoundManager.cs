45604f6517ebfd6ee2283978efdfafe73280f991
YU, RENE <s-RYU@lwsd.org>
2019-05-05 21:25:15 -0700

    tested sound, goodtogo

---
+++ Deploy/Assets/Scripts/Player/Ghost/GhostSoundManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSoundManager : MonoBehaviour
+{
+
+    public AudioSource ghostSound;
+
+    //Tier 2
+
+    public AudioClip DFCombined;
+
+    public AudioClip LeapInTheDark;
+
+    public AudioClip ShadowRush;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void playDFCombined()
+    {
+        ghostSound.clip = DFCombined;
+        ghostSound.PlayOneShot(DFCombined);
+        
+    }
+
+
+    void playLeapInTheDark()
+    {
+        ghostSound.clip = LeapInTheDark;
+        ghostSound.PlayOneShot(LeapInTheDark);
+        
+    }
+
+    void playShadowRush()
+    {
+
+    }
+
+    public void stop()
+    {
+        ghostSound.Stop();
+    }
+
+}
