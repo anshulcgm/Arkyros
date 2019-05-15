e3d7b06cd3ad1a45506084d1c24213e1ad112504
Anshul Ahluwalia <anshul.cgm@gmail.com>
2019-05-14 19:00:53 -0700

    Got pincer movement working on neutral terrain, have to eventually migrate to spherical surface

--- Deploy/Assets/Scripts/Enemies/Behavior/ShrabBehaviors/ShrabBehavior.cs
+++ Deploy/Assets/Scripts/Enemies/Behavior/ShrabBehaviors/ShrabBehavior.cs
@@ -9,9 +9,17 @@ public class ShrabBehavior : MonoBehaviour
     public float pincerMovementRange;
     public GameObject player;
 
+    public float shrabMovementSpeed; 
+
+    public GameObject leftPlayerLeg;
+    public GameObject rightPlayerLeg;
+
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>(); 
         this.shurikenRange = 15;
         this.shrabsInRange = new List<Shrab>();
         this.pincerMovementRange = 10;
@@ -20,6 +28,9 @@ public class ShrabBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.LookAt(player.transform);
+        pincerMovement();
+        /*
         foreach(Shrab s in Shrab.shrabList)
         {
             if(Vector3.Distance(s.referenceObject.transform.position, transform.position) < pincerMovementRange)
@@ -36,18 +47,52 @@ public class ShrabBehavior : MonoBehaviour
         {
             pincerMovement();
         }
+        */
     }
 
     public void pincerMovement()
     {
-        foreach (Shrab s in shrabsInRange)
+        if (Vector3.Distance(transform.position, leftPlayerLeg.transform.position) < Vector3.Distance(transform.position, rightPlayerLeg.transform.position))
         {
-            s.referenceObject.AddComponent<SphericalMovement>();
+            Debug.Log("going for right leg");
+            if (Vector3.Distance(transform.position, leftPlayerLeg.transform.position) >= 0.5f)
+            {
+
+                anim.SetBool("Attack", false);
+                float step = shrabMovementSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, leftPlayerLeg.transform.position, step);
+            }
+            else
+            {
+                anim.SetBool("Attack", true);
+            }
+        }
+        else
+        {
+            Debug.Log("going for left leg");
+            if (Vector3.Distance(transform.position, rightPlayerLeg.transform.position) >= 0.5f)
+            {
+                anim.SetBool("Attack", false);
+                float step = shrabMovementSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, rightPlayerLeg.transform.position, step);
+            }
+            else
+            {
+                anim.SetBool("Attack", true);
+            }
         }
     }
 
     public void waterShurikenAttack()
     {
+        
+    }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            Physics.IgnoreCollision(GetComponent<SphereCollider>(), collision.gameObject.GetComponent<Collider>());
+        }
     }
 }
