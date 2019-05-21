f082f8360b25132c1840dc6ad5072402fdbd1c5d
Shiven Bhatt <shivenbhatt@outlook.com>
2019-05-20 21:19:12 -0700

    terrain

---
+++ Deploy/Assets/renderSameSize.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+public class renderSameSize : MonoBehaviour
+{
+    public float scale;
+    public float baseSpeed;
+    public Camera player;
+    public AutoMoveAndRotate rotator;
+
+
+    float startSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startSize = (player.gameObject.transform.position - transform.position).magnitude;
+        baseSpeed = rotator.rotateDegreesPerSecond.value.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float size = (player.gameObject.transform.position - transform.position).magnitude;
+        gameObject.transform.localScale = new Vector3(size, size, size) * scale;
+        rotator.rotateDegreesPerSecond.value.x = baseSpeed * size/startSize;
+    }
+}
---
+++ Deploy/Assets/spawnStars.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+public class spawnStars : MonoBehaviour
+{
+    public GameObject star;
+    public Camera player;
+    public int numStars;
+    public float radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < numStars; i++){
+            GameObject starObj = Instantiate(star, UnityEngine.Random.onUnitSphere * radius, Quaternion.identity);
+            starObj.transform.parent = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
